b2cca01592565d97e3b0cd44ded9ed3130f84d97
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-01-11 23:12:22 +0300

    task1-4

--- src/Main.java
+++ src/Main.java
@@ -13,18 +13,22 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1");
+        for (int i =1; i <11; i++) System.out.print( i +", ");
     }
     public static void task2() {
-        System.out.println("Задача 2");
+        System.out.println("\n"+"Задача 2");
+        for (int i =10; i >0; i--) System.out.print(i +", ");
     }
     public static void task3() {
-        System.out.println("Задача 3");
+        System.out.println("\n"+"Задача 3");
+        for (int i =0; i <=17; i =i +2 ) System.out.print(i +", ");
     }
     public static void task4() {
-        System.out.println("Задача 4");
+        System.out.println("\n"+"Задача 4");
+        for (int i =10; i >=-10; i--) System.out.print(i+", ");
     }
     public static void task5() {
-        System.out.println("Задача 5");
+        System.out.println("\n"+"Задача 5");
     }
     public static void task6() {
         System.out.println("Задача 6");
